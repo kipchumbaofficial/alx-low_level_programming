15ed2070715e7a437fb2809b25467bb99ef80526
Victor kipchumba <victorkipchumba039@gmail.com>
2023-03-19 09:59:54 -0700

    Solution to Task 4: puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programing is like bulding a multilingual puzzle");
+	return (0);
+}
