b5e71279f42843a24cb613172309cee8c20944cb
Victor kipchumba <victorkipchumba039@gmail.com>
2023-03-20 01:53:51 -0700

    Updated solution to task 6: Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
 	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
-	printf("Size of a long: %lu byte(s)\n", sizeof(along));
-	printf("Size of a long long: %lu byte(s)\n", sizeof(alonglong));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 	return (0);
 }
