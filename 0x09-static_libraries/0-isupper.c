fee9ff0e283296a177f198b603d235a73aea1179
Victor kipchumba <victorkipchumba039@gmail.com>
2023-04-08 07:10:02 -0700

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _isupper - writes letters to upper case
+ *
+ * Return: 0
+ */
+int _isupper(int c)
+{
+}
--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/*
+ * main - checks code 
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
