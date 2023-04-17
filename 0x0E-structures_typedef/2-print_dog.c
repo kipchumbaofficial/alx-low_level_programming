fabbe0a36893070bf2d35d0c9ee9d1bf075c5ec2
Victor kipchumba <victorkipchumba039@gmail.com>
2023-04-17 09:52:01 -0700

    Updated Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,21 +12,21 @@ void print_dog(struct dog *d)
 	{
 		;
 	}
-	if (d->name == NULL)
+	if (d->name != NULL)
+	{
+		 printf("Name: %s\n", d->name);
+	}
+	else
 	{
 		printf("Name: (nil)\n");
 	}
-	else
-	{
-		printf("Name: %s\n", d->name);
-	}
-	printf("Age: %.1f\n", d->age);
-	if (d->owner == NULL)
-	{
-		printf("(nil)\n");
-	}
-	else
+	printf("Age: %f\n", d->age);
+	if (d->owner != NULL)
 	{
 		printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("(nil)\n");
 	}
 }
