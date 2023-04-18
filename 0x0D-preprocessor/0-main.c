63b51fd86af3fcdfac9ac52430b7cb8d0d51bc92
Victor kipchumba <victorkipchumba039@gmail.com>
2023-04-18 06:22:01 -0700

    Task 0: Object-like Macro

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,16 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+/**
+ * main - check code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int s;
+
+	s = 98 + SIZE;
+	printf("%d", s);
+	return (0);
+}
