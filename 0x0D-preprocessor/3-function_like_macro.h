9561caac690e9f32a7a25fed51b1e9230d4b9b35
Victor kipchumba <victorkipchumba039@gmail.com>
2023-04-18 07:34:58 -0700

    Solution to Task 3

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define ABS(x)\
+	do {\
+		while ((x) < 0)\
+		{\
+			(x) = (-1) * (x);\
+		} \
+	} while (0)
+#endif
---
+++ 0x0D-preprocessor/3-main.c
@@ -0,0 +1,17 @@
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
+#include <stdio.h>
+/**
+ * main - Checks code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j;
+
+	i = (-98) * 10;
+	j = 98 * 10;
+	printf("%d, %d\n", i, j);
+	return (0);
+}
