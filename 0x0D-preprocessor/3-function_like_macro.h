1561674b826aa67f91383bf663765fc2b74d93d0
Victor kipchumba <victorkipchumba039@gmail.com>
2023-04-18 09:11:34 -0700

    Task 3:  Function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#define ABS(x)\
-	do {\
-		if ((x) < 0)\
-		{\
-			(x) = (-1) * (x);\
-		} \
-	} while (0)
+
+#define ABS(x) (((x) < 0) ? (-(x)) : (x))
+
 #endif
--- 0x0D-preprocessor/3-main.c
+++ 0x0D-preprocessor/3-main.c
@@ -13,5 +13,13 @@ int main(void)
 	i = (-98) * 10;
 	j = 98 * 10;
 	printf("%d, %d\n", i, j);
+	printf("%d\n", ABS(1024));
+	printf("%d\n", ABS(-1024));
+	printf("%f\n", ABS(1024.827634));
+	printf("%f\n", ABS(-1024.827634));
+	printf("%ld\n", ABS(1022344827634));
+	printf("%ld\n", ABS(-1022344827634));
+	printf("%d\n", ABS(0));
+	printf("%d\n", ABS(1024 - 2048));
 	return (0);
 }
