b56404eacb830d652213fed306d2d8c959ce0eb2
Victor kipchumba <victorkipchumba039@gmail.com>
2023-04-18 00:33:04 -0700

    Correction To Task 2

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -7,11 +7,6 @@
  */
 int main(void)
 {
-	struct dog my_dog;
-
-	my_dog.name = NULL;
-	my_dog.age = .0f;
-	my_dog.owner = NULL;
-	print_dog(&my_dog);
+	print_dog(NULL);
 	return (0);
 }
--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,24 +9,25 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-	{
 		;
-	}
-	if (d->name != NULL)
-	{
-		printf("Name: %s\n", d->name);
-	}
 	else
 	{
-		printf("Name: (nil)\n");
-	}
-	printf("Age: %f\n", d->age);
-	if (d->owner != NULL)
-	{
-		printf("Owner: %s\n", d->owner);
-	}
-	else
-	{
-		printf("Owner: (nil)\n");
+		if (d->name != NULL)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+		printf("Age: %f\n", d->age);
+		if (d->owner != NULL)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
 	}
 }
