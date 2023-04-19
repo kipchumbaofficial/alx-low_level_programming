f5f1dbac6d582cb64d286ee428db24f5acf85382
Victor kipchumba <victorkipchumba039@gmail.com>
2023-04-19 07:07:27 -0700

    Updated  Task 0: What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
  * print_name - Prints name
  * @name: name to be printed
@@ -6,5 +7,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name != NULL && *name != '\0' && f != NULL)
 	f(name);
 }
