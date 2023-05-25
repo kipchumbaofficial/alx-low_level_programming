42ae7518c4346b9339531cf8aee70d3231f4f443
Victor kipchumba <victorkipchumba039@gmail.com>
2023-05-25 02:15:48 -0700

    Task 6:  Half the lies they tell about me aren't true - Long string correction

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,23 @@
 #include "main.h"
 /**
+ * str_len - Gets length of the given string
+ * @str: pointer to string
+ *
+ * Return: length
+ */
+int str_len(char *str)
+{
+	int len;
+
+	len = 0;
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+/**
  * puts2 - Prints string incremented by 2
  * @str: Pointer to string to be printed
  *
@@ -7,10 +25,16 @@
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int i;
+	int len;
+
+	len = str_len(str);
+	for (i = 0; i <= len; i++)
 	{
-		_putchar(*str);
-		str += 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
