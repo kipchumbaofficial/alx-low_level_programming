4c9fd1284e6efc7ec3403d0c01dea88ea83bf39f
Victor kipchumba <victorkipchumba039@gmail.com>
2023-05-30 00:43:30 -0700

    Task 9: strcpy

---
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Checks code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char s1[98];
+	char *ptr;
+
+	ptr = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+	printf("%s\n", s1);
+	printf("%s\n", ptr);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * str_len - Gets length of a string
+ * @str: Pointer to the string
+ *
+ * Return: Length
+ */
+int str_len(char *str)
+{
+	int len;
+
+	len = 0;
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+/**
+ * _strcpy - Copy string from src to dest
+ * @src: Sources
+ * @dest: destination
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, len;
+
+	len = str_len(src);
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
