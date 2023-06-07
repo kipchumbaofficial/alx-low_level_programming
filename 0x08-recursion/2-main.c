a9d877be75dd180e888b4f900cefc63a4d376b5b
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-06-07 07:19:13 -0700

    Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Checks code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
+}
