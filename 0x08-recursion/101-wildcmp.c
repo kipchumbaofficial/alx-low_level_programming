b5dc1ffff1fdd0a69397343c9d12c53f57cd70e5
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-06-08 09:30:39 -0700

    Task 8: Betty correction

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,7 +16,7 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp(s1, s2 + 1));
 	}
-	else if(*s1 != '\0' && wildcmp(s1 + 1, s2))
+	else if (*s1 != '\0' && wildcmp(s1 + 1, s2))
 	{
 		return (1);
 	}
