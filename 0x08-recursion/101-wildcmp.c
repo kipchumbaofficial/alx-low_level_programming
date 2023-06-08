296316bf244c9cef344f5ff841bf402a04bf3678
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-06-08 09:28:04 -0700

    Task 8: Correction

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,9 +12,13 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (1);
 	}
-	else if (*s1 == '\0' || *s2 == '\0')
+	else if (*s2 == '*')
 	{
-		return (0);
+		return (wildcmp(s1, s2 + 1));
+	}
+	else if(*s1 != '\0' && wildcmp(s1 + 1, s2))
+	{
+		return (1);
 	}
 	else if (*s1 == *s2)
 	{
