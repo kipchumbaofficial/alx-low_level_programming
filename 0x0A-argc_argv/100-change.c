cab6e2024da5a622a73b77a363e756bf68aed108
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-06-10 00:59:44 -0700

    Task 5:Minimal Number of Coins for Change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * minChange - Checks minimum change to return
+ * from passed coins
+ * @cents: Number passed
+ */
+void minChange(int cents)
+{
+	if (cents < 0)
+	{
+		printf("%d\n", 0);
+	}
+	else if (cents <= 10)
+	{
+		printf("%d\n", 1);
+	}
+	else if (cents <= 25 && cents > 10)
+	{
+		printf("%d\n", 3);
+	}
+	else if (cents <= 100 && cents > 25)
+	{
+		printf("%d\n", 4);
+	}
+	else
+	{
+		printf("%d\n", 5);
+	}
+}
+/**
+ * main - Passes Arguments
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int cents;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		cents = atoi(argv[1]);
+		minChange(cents);
+	}
+	return (0);
+}
