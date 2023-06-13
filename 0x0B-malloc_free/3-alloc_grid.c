e49b6699471c6fffae1e8697219b1f8844b53743
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-06-13 06:14:27 -0700

    Task 3: If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * alloc_grid - Allocates space to 2d array
+ * @width: Width of matrix
+ * @height: Height of matrix
+ *
+ * Return: 2D Array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width < 1 || height < 1)
+	{
+		return (NULL);
+	}
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
---
+++ 0x0B-malloc_free/3-main.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_grid - prints grid of integers
+ * @grid: Address of grid
+ * @height: Height of the grid
+ * @width: Width of the grid
+ */
+void print_grid(int **grid, int width, int heigth)
+{
+	int w;
+	int h;
+
+	h = 0;
+	while (h < heigth)
+	{
+		w = 0;
+		while (w < width)
+		{
+			printf("%d ", grid[h][w]);
+			w++;
+		}
+		printf("\n");
+		h++;
+	}
+}
+/**
+ * main - Checks code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int **grid;
+
+	grid = alloc_grid(6, 4);
+	if (grid == NULL)
+	{
+		return (1);
+	}
+	print_grid(grid, 6, 4);
+	printf("\n");
+	grid[0][3] = 98;
+	grid[3][4] = 402;
+	print_grid(grid, 6, 4);
+	return (0);
+}
