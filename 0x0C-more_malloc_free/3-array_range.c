007ff149668d557fd221aa546ebe4af8b5ba87cc
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-06-16 03:07:59 -0700

    Task 3: array_range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+/**
+ * array_range - Creates an array of integers
+ * @min: Least number
+ * @max: High most number
+ *
+ * Return: Pointer to array crated
+ */
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	ptr = malloc((max + 1) * sizeof(int));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (min <= max && i <= max)
+	{
+		*(ptr + i) = min++;
+		i++;
+	}
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/3-main.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+/**
+ * simple_print_buffer - Prints buffer in Hexa
+ * @buffer: Memory address
+ * @size: Size of the address
+ *
+ * Return: Nothing
+ */
+void simple_print_buffer(int *buffer, unsigned int size)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n");
+}
+/**
+ * main - Checks code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int *a;
+
+	a = array_range(10, 20);
+	simple_print_buffer(a, 11);
+	free(a);
+	return (0);
+}
