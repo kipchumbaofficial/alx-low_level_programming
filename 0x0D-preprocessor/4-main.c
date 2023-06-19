182157ef0718bc9218fe613d4933fd9b9be6efd2
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-06-19 08:20:49 -0700

    Task 4: SUM

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,16 @@
+#include "4-sum.h"
+#include "4-sum.h"
+#include <stdio.h>
+/**
+ * main - Checks code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int s;
+
+	s = SUM(98, 1024);
+	printf("%d\n", s);
+	return (0);
+}
