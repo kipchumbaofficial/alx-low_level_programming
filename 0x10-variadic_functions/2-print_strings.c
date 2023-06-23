6661038447ba96c8fcf06c5cd7f6188b479e1047
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-06-23 04:48:18 -0700

    Task 2: Correction

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,35 +12,32 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	const char *str;
 	va_list args;
 
-	if (n > 0)
+	if (separator == NULL)
 	{
-		if (separator == NULL)
+		va_start(args, n);
+		for (i = 0; i < n; i++)
 		{
-			va_start(args, n);
-			for (i = 0; i < n; i++)
-			{
-				str = va_arg(args, const char *);
-				if (str == NULL)
-					printf("(nil)");
-				else
-					printf("%s", str);
-			}
-			va_end(args);
-			printf("\n");
+			str = va_arg(args, const char *);
+			if (str == NULL)
+				printf("(nil)");
+			else
+				printf("%s", str);
 		}
-		else
+		va_end(args);
+		printf("\n");
+	}
+	else
+	{
+		p = n - 1;
+		va_start(args, n);
+		for (i = 0; i < p; i++)
 		{
-			p = n - 1;
-			va_start(args, n);
-			for (i = 0; i < p; i++)
-			{
-				str = va_arg(args, const char *);
-				if (str == NULL)
-					printf("(nil)%s", separator);
-				else
-					printf("%s%s", str, separator);
-			}
-			printf("%s\n", va_arg(args, const char *));
+			str = va_arg(args, const char *);
+			if (str == NULL)
+				printf("(nil)%s", separator);
+			else
+				printf("%s%s", str, separator);
 		}
+		printf("%s\n", va_arg(args, const char *));
 	}
 }
