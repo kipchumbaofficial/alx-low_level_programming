08fa8c72add8ecdafcd44e3897bbf6196f046877
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-06-30 09:55:38 -0700

    Task 2: Add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * _strlen - Gets length of the string
+ * @str: String
+ *
+ * Return: Length of the string
+ */
+unsigned int _strlen(const char *str)
+{
+	if (*str == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen(str + 1));
+}
+/**
+ * add_node - Adds a node at the beginning of list_t
+ * @head: Pointer to head of the list
+ * @str: String
+ *
+ * Return: Pointer to address of the new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newnode;
+
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+	newnode->str = strdup(str);
+	newnode->len = _strlen(str);
+	newnode->next = *head;
+	*head = newnode;
+
+	return(newnode);
+}
