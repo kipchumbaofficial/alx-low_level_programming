6647868afa4877b1629c2928b82a2f9e66691e83
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-07-02 02:56:30 -0700

    Task 2: Fixed betty errors

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "dog.h"
 /**
- * print_day - Prints a struct dog
+ * print_dog - Prints a struct dog
  * @d: Pointer to structure to print
  */
 void print_dog(struct dog *d)
@@ -16,13 +16,6 @@ void print_dog(struct dog *d)
 		{
 			d->owner = "(nil)";
 		}
-		if (d->age != 0)
-		{
-			printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
-		}
-		else
-		{
-			printf("Name: %s\nAge: %s\nOwner: %s\n", d->name, "(nil)", d->owner);
-		}
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
 }
