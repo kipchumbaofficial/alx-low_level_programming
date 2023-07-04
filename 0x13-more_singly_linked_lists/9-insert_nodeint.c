49ce96912c9dcd22282067b5ddfadd938796f578
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-07-04 09:07:30 -0700

    Task 9: Insert correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		while (start != NULL)
 		{
-			if (count == idx)
+			if (count == (idx - 1))
 			{
 				new->next = start->next;
 				start->next = new;
