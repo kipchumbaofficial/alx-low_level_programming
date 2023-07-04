e66498599a2c5b753d48635ffeb397173a24de47
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-07-04 05:40:03 -0700

    Task 6: Pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * pop_listint - Deletes head node of listint_t list
+ * @head: Reference
+ *
+ * Return: 0 or data of head node
+ */
+int pop_listint(listint_t **head)
+{
+	int i = 0;
+	listint_t *start, *nxt;
+
+	if (head != NULL && head != NULL)
+	{
+		start = *head;
+		i = start->n;
+		nxt = start->next;
+		start = nxt;
+		*head = start;
+	}
+	return (i);
+}
