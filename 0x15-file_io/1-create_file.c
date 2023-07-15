d256236d5460c2304911d5dc82a0a190ae05c49e
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-07-14 23:28:57 -0700

    TAsk 1: Correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -38,7 +38,7 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 	{
 		return (-1);
