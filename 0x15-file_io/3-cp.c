b066f8abd1c8f6a1691abcaea2f38dd015e7a3c5
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-07-26 08:17:41 -0700

    Task 3: cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,78 @@
+#include "main.h"
+/**
+ * _printerror - Handles errors
+ * @status: Exit status
+ * @filename: Name of file
+ */
+void _printerror(int status, char *filename)
+{
+	if (status == 98)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	if (status == 99)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(98);
+	}
+
+}
+/**
+ * copier - Copies contet from src to dest
+ * @src: Source
+ * @dest: Destination
+ *
+ * Return: 1 on success -1 on failure
+ */
+void copier(char *src, char *dest)
+{
+	int fd_read, res_read, fd_write, res_write;
+	char *buf[1024];
+
+	fd_read = open(src, O_RDONLY);
+	if (fd_read < 0)
+		_printerror(98, src);
+	fd_write = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_write < 0)
+	{
+		close(fd_read);
+		_printerror(99, dest);
+	}
+	do {
+		res_read = read(fd_read, buf, 1024);
+		if (res_read < 0)
+		_printerror(98, src);
+		res_write = write(fd_write, buf, res_read);
+		if (res_write < res_read)
+			_printerror(99, dest);
+	} while (res_write == 1024);
+	if (close(fd_read) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_read);
+		close(fd_write);
+		exit(100);
+	}
+	if (close(fd_write) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_write);
+		exit(100);
+	}
+}
+/**
+ * main - Entry point
+ * @ac: Argument count
+ * @av: Argument Vector
+ *
+ * Return: 0
+ */
+int main(int ac, char *av[])
+{
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	copier(av[1], av[2]);
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,6 +4,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
