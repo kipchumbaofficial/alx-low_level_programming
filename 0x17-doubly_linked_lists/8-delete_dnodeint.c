3bfeb5d20cfdce26a727d26bfce770e4eaea13ce
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-08-18 02:15:45 -0700

    Task 8: Correcting cause of segfault

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,10 +9,14 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
-	dlistint_t *temp = *head;
+	dlistint_t *temp;
+
+	if (head == NULL)
+		return (-1);
 
 	if (head != NULL && *head != NULL)
 	{
+		temp = *head;
 		if (index == 0)
 		{
 			*head = (*head)->next;
--- 0x17-doubly_linked_lists/8-main.c
+++ 0x17-doubly_linked_lists/8-main.c
@@ -26,7 +26,7 @@ int main(void)
     delete_dnodeint_at_index(&head, 5);
     print_dlistint(head);
     printf("-----------------\n");
-    delete_dnodeint_at_index(&head, 0);
+    delete_dnodeint_at_index(NULL, 0);
     print_dlistint(head);
     printf("-----------------\n");
     delete_dnodeint_at_index(&head, 0);
