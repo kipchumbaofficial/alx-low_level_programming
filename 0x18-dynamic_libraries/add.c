27abfbf8d8f5e881cda9178bbfc7a1cea1537e31
Victor Kipchumba <victorkipchumba039@gmail.com>
2023-08-21 11:00:37 -0700

    Task 2: Let's call C functions from Python

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,55 @@
+/**
+ * add - Sums int
+ * @a: First int
+ * @b: 2nd int
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - Gets dufference
+ * @a: 1st int
+ * @b: 2nd int
+ *
+ * Return: diff
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - Multiplies 2 int
+ * @a: First int
+ * @b: 2nd int
+ *
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - Gets quotient
+ * @a: 1st integer
+ * @b: 2nd integer
+ *
+ * Return: quotient
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - Gets remainder
+ * @a: 1st int
+ * @b: 2nd int
+ *
+ * Return: remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
